ce1dc8c31e17976c6a2f65b20d4fafe8d4c89eb3
Anneke Anderson <anneke_anderson@icloud.com>
2024-10-26 13:19:23 -0500

    Fix react app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import HomePage from './HomePage';
+import Navbar from './Navbar';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1 class="header">DAM Creative Tools</h1>
+      <p>Homepage</p>
+    </div>
+  );
+}
+
+export default HomePage;
