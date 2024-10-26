04426b4969c2a6b0f2a04c39175398398af972f1
sophiaptran <sophiatran1316@gmail.com>
2024-10-26 14:36:12 -0500

    login page

--- frontend/ui-ux/src/App.js
+++ frontend/ui-ux/src/App.js
@@ -1,16 +1,40 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 import Navbar from './Navbar';
+import Login from './login';
 
 
-function App() {
+
+/*function App() {
   return (
+    <div>
+      <Login />
+      
+    </div>
+  );
+}
+
+function HomePage() {
+  return(
     <div>
       <Navbar />
       <HomePage />
     </div>
+  )
+}*/
+
+const App = () => {
+  return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<HomePage />} />  {/* Use HomePage here */}
+          </Routes>
+      </Router>
   );
-}
+};
+
 
 export default App;
